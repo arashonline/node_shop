6d7065f5b04758c90acc4e8318c3c4d8d69786a5
arash <arash.rabiee.88@gmail.com>
2019-12-08 23:59:12 +0330

    adding express.js to project \n using middleware\n  using next()

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+// adding express framework to project
+
+const express = require('express');
+
+// creating an express app
+
+const app = express();
+
+// we can use some middleware 
+// most simple way is to use a function 
+// req =>
+// res =>
+// next => allow request to go to next middleware
+app.use((req, res, next)=>{
+    console.log('in The middleware')
+    next();
+});
+app.use((req, res, next)=>{
+    console.log('In The Second Middleware')
+});
+
+// app is also a valid request handler so we can pass it to server
+
+const server = http.createServer(app);
+server.listen(8020);
