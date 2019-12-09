03e068677a39ea4bc14028215f224c5d07a40fb9
arash <arash.rabiee.88@gmail.com>
2019-12-10 02:38:01 +0330

    some code clean up

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-const port = 8020;
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs',expressHbs({layoutsDir:'views/layouts/',defaultLayout:'main-layout',extname:'hbs'}));
-
-// setting a global configuration value
-// app.set('view engine', 'pug');
-// app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
-// then we set views folder, the default one is MainFolder/views
-app.set('views','views');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const rootDir = require('./util/path')
 
-app.use(bodyParser.urlencoded({extended:false}))
-// serve file statically 
-// grant read access to public folder
-app.use(express.static(path.join(rootDir,'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// admin routes is a middleware
-// add a segment as filter for all routes
-app.use('/admin',adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-// handling 404 
-app.use((req,res,next)=>{
-    // chaining status to send
-    // res.status(404).sendFile(path.join(rootDir,'views','404.html'))
-    res.render('404',{pageTitle:"Page Not Found"})
-})
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path:'' });
+  });
 
-app.listen(port)
+app.listen(8020);
--- routes/shop.js
+++ routes/shop.js
@@ -13,10 +13,6 @@ const adminData = require('./admin');
 
 // now we use the router to register things
 router.get('/', (req, res, next) => {
-    // console.log(adminData.products)
-    // res.sendFile(path.join(rootDir,'views','shop.html'))
-
-    // looking for the shop.pug in the defined views folder (in app.js we defined that)
 
     const products = adminData.products
 
