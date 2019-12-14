7748986409561b7d96bff17b689a246da4a3d149
arash <arash.rabiee.88@gmail.com>
2019-12-14 04:22:03 +0330

    Edit product is okay

--- routes/admin.js
+++ routes/admin.js
@@ -12,11 +12,12 @@ const adminController = require('../controllers/admin');
 // now we use the router to register things
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
-router.get('/edit-product', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 router.get('/products',adminController.getProductList);
 
 // /admin/add-product => POST
 router.post('/add-product',adminController.postAddProduct)
+router.post('/update-product',adminController.postEditProduct)
 
 
 
