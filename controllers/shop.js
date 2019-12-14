b369001816cf4787c91c0de1e7e07c60d268f91b
arash <arash.rabiee.88@gmail.com>
2019-12-14 05:27:35 +0330

    add to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -37,10 +37,23 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        pageTitle: "Cart page",
-        path: '/cart',
+    Cart.getCart(cart =>{
+        Product.fetchAll(products =>{
+            const cartProducts = []
+            for(product of products){
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if(cartProductData){
+                    cartProducts.push({productData: product, qty:cartProductData.qty} );
+                }
+            }
+            res.render('shop/cart', {
+                pageTitle: "Cart page",
+                path: '/cart',
+                products: cartProducts,
+            })
+        });
     })
+    
 }
 
 exports.postCart = (req, res, next) => {
