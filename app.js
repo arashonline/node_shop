529a60e013d2bdd6bf173fe02fa0b4fa33f00381
arash <arash.rabiee.88@gmail.com>
2019-12-18 00:48:53 +0330

    sequelize create table for us

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -23,4 +23,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(8020);
+// sync method looks at all models
+// It syncs models to the database
+sequelize.sync().then(result =>{
+    // console.log(result);
+    app.listen(8020);
+
+})
+.catch(err=>{
+    console.log(err);
+});
+
