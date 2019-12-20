e594cf98f986620fba8f19ce47b5c550943736bd
arash <arash.rabiee.88@gmail.com>
2019-12-20 13:22:00 +0330

    cart and cart item added

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -40,8 +42,14 @@ app.use(errorController.get404);
 
 // defining associations (Relations)
 
-Product.belongsTo(User,{constraints:true, onDelete:'CASCADE'});
-User.hasMany(Product)
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 // sync method looks at all models
 // It syncs models to the database
