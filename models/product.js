7c0539a5d1fd9aa38743a9c2f992656a7d447d02
arash <arash.rabiee.88@gmail.com>
2019-12-20 22:36:42 +0330

    saving a product to database

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ class Product {
   save() {
     const db = getDb();
     // insertOne get an object to insert to db
-    db.collection('products').insertOne(this)
+    return db.collection('products').insertOne(this)
       .then(result =>{
         console.log(result);
       })
