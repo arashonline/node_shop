2e52799da8c75fb48ee139a70b08ef0507757149
arash <arash.rabiee.88@gmail.com>
2019-12-20 22:12:28 +0330

    using one connection to db from various point in our app

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 
@@ -32,13 +32,12 @@ app.use((req, res, next) => {
     // .catch(err=>{console.log(err)});
 })
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
 // mongodb 
 
-mongoConnect((client) => {
-    console.log(client);
+mongoConnect(() => {
     app.listen(2081);
 })
--- util/database.js
+++ util/database.js
@@ -1,15 +1,33 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
 
+// to have one connection which is possible to connect from all over of our application 
+// we use _db 
+let _db;
+
 const mongoConnect = (callback)=>{
     MongoClient.connect('mongodb://localhost:27017/nodeShop')
     .then(client =>{
         console.log('Connected!');
+        // nodeShop below overwrite the db_name we used above 
+        // don't need to create db before hand, it will created on the fly
+        _db = client.db('nodeShop');
         callback(client);
     })
     .catch(err=>{
         console.log(err);
+        throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = ()=>{
+    if(_db){
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+// for connecting to the database
+exports.mongoConnect = mongoConnect;
+// for using the connection
+exports.getDb = getDb;
